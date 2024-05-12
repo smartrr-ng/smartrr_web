a6451fa388abf27e6bfccdeedd9e8b3b1d4f65f8
ibukunoluwanap <promisenap2001@gmail.com>
2024-05-12 17:12:11 +0300

    done

---
+++ app/(application)/privacy/page.js
@@ -0,0 +1,16 @@
+"use client";
+
+import Navbar from "@/app/_components/navigations/navbar";
+import Footer from "@/app/_components/navigations/footer";
+
+const PrivacyPage = () => {
+  return (
+    <main>
+      <Navbar />
+
+      <Footer />
+    </main>
+  );
+};
+
+export default PrivacyPage;
