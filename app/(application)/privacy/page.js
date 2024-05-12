31c81f263727491e1961d951159133989f9ce919
ibukunoluwanap <promisenap2001@gmail.com>
2024-05-12 17:27:53 +0300

    upd

--- app/(application)/privacy/page.js
+++ app/(application)/privacy/page.js
@@ -3,7 +3,14 @@ import Footer from "@/app/_components/navigations/footer";
 import Privacy from "@/app/_components/privacy";
 
 export async function generateMetadata() {
-  return { title: "SmartRR - Privacy" };
+  return {
+    title: "SmartRR - Privacy",
+    openGraph: {
+      title: "SmartRR - Privacy",
+      url: "https://smartrrapp.org/privacy",
+    },
+    metadataBase: new URL("https://smartrrapp.org/privacy"),
+  };
 }
 
 const PrivacyPage = () => {
--- app/(application)/terms/page.js
+++ app/(application)/terms/page.js
@@ -3,7 +3,14 @@ import Footer from "@/app/_components/navigations/footer";
 import Terms from "@/app/_components/terms";
 
 export async function generateMetadata() {
-  return { title: "SmartRR - Terms & Conditions" };
+  return {
+    title: "SmartRR - Terms & Conditions",
+    openGraph: {
+      title: "SmartRR - Terms & Conditions",
+      url: "https://smartrrapp.org/terms",
+    },
+    metadataBase: new URL("https://smartrrapp.org/terms"),
+  };
 }
 
 const TermsPage = () => {
